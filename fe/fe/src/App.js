15b34eb3a1006c98945e18d0c2f1ef8a73b56ed7
Dev <dev@Devs-MacBook-Pro.local>
2023-05-15 16:31:35 -0700

    React Stuff

--- fe/fe/src/App.js
+++ fe/fe/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header'
 import Main from './components/form/main'
+import Footer from './components/footer/footer'
 
 function App() {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
