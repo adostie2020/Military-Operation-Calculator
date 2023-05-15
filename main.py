520df2e605ac95a85979293a2f1f32da9c5be099
CosmicDev-Official <118325755+CosmicDev-Official@users.noreply.github.com>
2023-05-15 22:06:48 +0000

    Updated react code and added Duffel API stuff

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from duffel_api import Duffel
 from flask import Flask
 
 app = Flask(__name__, static_folder="fe/fe/build", static_url_path="")
-
+client = Duffel(access_token = "duffel_test_wR7qOeLxoMTMziq7CGRJcKR6as2tvwQnuMoVVR0ESfj")
 
 @app.route("/api/route1")
 def main():
