5bb19e0c93948a5f7c8a8d0e9fc8d79ced63f39a
CosmicDev-Official <118325755+CosmicDev-Official@users.noreply.github.com>
2023-05-15 21:52:19 +0000

    Updated Flask file

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ from flask import Flask
 
 app = Flask(__name__, static_folder="fe/fe/build", static_url_path="")
 
+
 @app.route("/api/route1")
 def main():
-    # API code here
-    return "Hello, World! This is a test"
-
-
-
-
+  # API code here
+  return "Hello, World! This is a test"
 
 
 # serve the react routes
 @app.route("/", defaults={"path": ""})
 def react(path):
-    return app.send_static_file("index.html")
+  return app.send_static_file("index.html")
+
 
 app.run(host="0.0.0.0", port=8000)
