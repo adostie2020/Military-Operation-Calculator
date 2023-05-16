7d14b76df8762ffe51bfbb453c9bed82a3316a65
Martin Lizarraga <martin24blr@gmail.com>
2023-05-16 15:59:10 -0700

    Add new aircraft table

--- fe/fe/src/components/aircrafttable/aircrafttable.js
+++ fe/fe/src/components/aircrafttable/aircrafttable.js
@@ -1,56 +1,63 @@
 import React from 'react';
 import AircraftTable from '../home/AircraftTable-Page';
-import Swal from 'sweetalert2'
-
+import Swal from 'sweetalert2';
+import axios from 'axios'
+import './aircrafttable.css'
 
 class TheAircraftTable extends React.Component {
     constructor(props) {
         super(props);
     }
 
-    swalfirestuff() {
+    async swalfirestuff() {
         Swal.fire({
-            title: 'Submit your Github username',
-            input: 'text',
-            inputAttributes: {
-                autocapitalize: 'off'
-            },
-            showCancelButton: true,
-            confirmButtonText: 'Look up',
-            showLoaderOnConfirm: true,
-            preConfirm: (login) => {
-                return fetch(`//api.github.com/users/${login}`)
-                    .then(response => {
-                        if (!response.ok) {
-                            throw new Error(response.statusText)
-                        }
-                        return response.json()
+            title: 'New Aircraft Info',
+            html:
+                '<input id="input1" class="swal2-input" placeholder="Enter Aircraft name">' +
+                '<input id="input2" class="swal2-input swal2-number-input" type="number" placeholder="Aircraft Amount">' +
+                '<input id="input3" class="swal2-input swal2-number-input" type="number" placeholder="Personnel Number">',
+            focusConfirm: false,
+            preConfirm: () => {
+                const newName = document.getElementById('input1').value;
+                const newAmount = document.getElementById('input2').value;
+                const newPersonnel = document.getElementById('input3').value;
+                console.log('Input 1:', newName);
+                console.log('Input 2:', newAmount);
+                console.log('Input 3:', newPersonnel);
+
+                axios
+                    .post('https://hackathon-pacaf.thecosmoking.repl.co/api/add_aircraft', {
+                        type: newName,
+                        number: parseInt(newAmount),
+                        personnel: parseInt(newPersonnel)
                     })
-                    .catch(error => {
-                        Swal.showValidationMessage(
-                            `Request failed: ${error}`
-                        )
+                    .then((response) => {
+                        // Handle the response
+                        console.log('Post request successful:', response.data);
                     })
+                    .catch((error) => {
+                        // Handle the error
+                        console.error('Error making post request:', error);
+                    });
+
+
             },
-            allowOutsideClick: () => !Swal.isLoading()
-        }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire({
-                    title: `${result.value.login}'s avatar`,
-                    imageUrl: result.value.avatar_url
-                })
+            didOpen: () => {
+                const numberInputs = document.querySelectorAll('.swal2-number-input');
+                numberInputs.forEach((input) => {
+                    input.classList.add('swal2-input');
+                });
             }
-        })
+        });
     }
 
     render() {
         return (
             <div>
-                {this.swalfirestuff(this)}
+                <button onClick={() => this.swalfirestuff()}>Add New Aircraft</button>
             </div>
         );
     }
-
 }
 
-export default TheAircraftTable
+export default TheAircraftTable;
