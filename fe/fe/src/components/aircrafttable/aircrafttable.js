d764ef18c0c6e98d53265fe67fe2b49bfeecc368
Martin Lizarraga <martin24blr@gmail.com>
2023-05-16 14:15:05 -0700

    Button stuff

--- fe/fe/src/components/aircrafttable/aircrafttable.js
+++ fe/fe/src/components/aircrafttable/aircrafttable.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import AircraftTable from '../home/AircraftTable-Page';
+import Swal from 'sweetalert2'
 
-function TheAircraftTable(){
-    return(
-        <div>
-            APSDFJSPO
-        </div>
-    );
+
+class TheAircraftTable extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    swalfirestuff() {
+        Swal.fire({
+            title: 'Submit your Github username',
+            input: 'text',
+            inputAttributes: {
+                autocapitalize: 'off'
+            },
+            showCancelButton: true,
+            confirmButtonText: 'Look up',
+            showLoaderOnConfirm: true,
+            preConfirm: (login) => {
+                return fetch(`//api.github.com/users/${login}`)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(response.statusText)
+                        }
+                        return response.json()
+                    })
+                    .catch(error => {
+                        Swal.showValidationMessage(
+                            `Request failed: ${error}`
+                        )
+                    })
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: `${result.value.login}'s avatar`,
+                    imageUrl: result.value.avatar_url
+                })
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.swalfirestuff(this)}
+            </div>
+        );
+    }
+
 }
 
 export default TheAircraftTable
